e8004fe15708ba3ab26ecad033b6af51aa20d89a
arlynch435 <arlynch@NN123-HA-03.HS.sd203.org>
2015-11-04 14:12:58 -0600

    Moved on to Armstrong numbers

--- Chapter 6 Practice/AreaUnderCurve.java
+++ Chapter 6 Practice/AreaUnderCurve.java
@@ -1,27 +1,93 @@
 import java.util.Random;
 public class AreaUnderCurve
 {
-    public static void main(String[] args)
+    public double A;
+    public double B;
+    public double C;
+    public double XRight;
+    public double XLeft;
+    public double YTop;
+    public double YBottom;
+    public AreaUnderCurve(double A, double B, double C,double XRight,double XLeft)
     {
-        //let's go boys!
-        final int TRIES=1000000;
-        Random generator= new Random();
-        int hits=0;
-        for (int i=1;
-                i<=TRIES;
-                i++)
+        this.A=A;
+        this.B=B;
+        this.C=C;
+        this.XRight=XRight;
+        this.XLeft=XLeft;
+    }
+    public boolean isAPositive()
+    {
+        if (this.A>0)
         {
-            double r=generator.nextDouble();
-            double x=-1+2*r;
-            r=generator.nextDouble();
-            double y=-1+2*r;
-            if ((x*x)+(y*y)<=1)
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public boolean isANegative()
+    {
+        if (this.A<0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public boolean isAZero()
+    {
+        if (this.A>=0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public double func(double pointx)
+    {
+        double pointy=0;
+        pointy=(this.A*pointx*pointx)+(this.B*pointx)+this.C;
+        return pointy;
+    }
+    public double findExtrema()
+    {
+        double extremay=0;
+        extremay=(-this.B)/(2*this.A);
+        return extremay;
+    }
+    public void boundaryBox()
+    {
+        //a positive, extrema positive
+        if (this.isAPostive()==true)
+        {
+            if (this.findExtrema>0)
             {
-                hits++;
+                
+            }
+            else
+            //a positive extrema negative
+            {
+                this.YBottom=this.findExtrema();
+                double choice1=this.func(this.XRight);
+                double choice2=this.func(this.XLeft);
+                if (choice1>choice2)
+                {
+                    this.YTop=choice1;
+                }
+                else if (choice1<choice2)
+                {
+                    this.YTop=choice2;
+                }
             }
         }
-        double piEstimate=4.0*hits/TRIES;
-        System.out.println("The estimate for pi is: "+piEstimate);
+        //a negative extrema negative
+        //a negative extrema positive
+        
     }
-
 }
---
+++ Chapter 6 Practice/AreaUnderCurveCaller.java
@@ -0,0 +1,9 @@
+
+public class AreaUnderCurveCaller
+{
+   public static void main(String[] args)
+    {
+        AreaUnderCurve function=new AreaUnderCurve(1.1,2.2,3.3,4.4,5.5);
+
+    }
+}
